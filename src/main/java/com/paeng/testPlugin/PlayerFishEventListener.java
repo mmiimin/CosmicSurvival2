e7581b1a31f7d0db07ec66a768797d9bfbc7e82c
Jiwon <57394364+JiwonPaeng@users.noreply.github.com>
2024-10-26 13:07:35 +0900

    Content Update

--- src/main/java/com/paeng/testPlugin/PlayerFishEventListener.java
+++ src/main/java/com/paeng/testPlugin/PlayerFishEventListener.java
@@ -3,6 +3,8 @@ package com.paeng.testPlugin;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.*;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Item;
@@ -10,7 +12,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -61,12 +65,13 @@ public class PlayerFishEventListener implements Listener {
         int weight_item = (int) (Math.random() * 100);
 
         int item_class_code;
-        String[] item_class_names = {"FISH", "COMMON", "RARE", "EPIC", "LEGENDARY"};
+        String[] item_class_names = {"FISH", "COMMON", "RARE", "EPIC", "LEGENDARY","SPECIAL"};
         if (weight_class < 800) item_class_code = 0; // FISH : 80.0%
-        else if (weight_class < 900) item_class_code = 1; // COMMON : 10.0% -> 10%
-        else if (weight_class < 995) item_class_code = 2; // RARE : 9.5% -> 9.5%
-        else if (weight_class < 999) item_class_code = 3; // EPIC : 0.45% -> 0.4%
-        else item_class_code = 4; // LEGENDARY : 0.05% -> 0.1%
+        else if (weight_class < 900) item_class_code = 1; // COMMON : 10.0%
+        else if (weight_class < 990) item_class_code = 2; // RARE : 9.0%
+        else if (weight_class < 997) item_class_code = 3; // EPIC : 0.7%
+        else if (weight_class < 999) item_class_code = 4; // LEGENDARY : 0.2%
+        else item_class_code = 5; // SPECIAL : 0.1%
 
         int mainHandLevel = 0, offHandLevel = 0, luckEnchantmentLevel = 0;
         try { mainHandLevel = event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getEnchantLevel(Enchantment.LUCK_OF_THE_SEA); }
@@ -105,15 +110,17 @@ public class PlayerFishEventListener implements Listener {
         }
 
         // 1. Play Sound
-        float[] pitch_data = {0.5F, 0.5F, 2.0F, 0.5F, 1.0F};
-        Sound[] sound_data = {Sound.ITEM_BUCKET_FILL_FISH, Sound.BLOCK_NOTE_BLOCK_BASS, Sound.BLOCK_NOTE_BLOCK_BELL, Sound.ENTITY_PLAYER_LEVELUP, Sound.UI_TOAST_CHALLENGE_COMPLETE};
+        float[] pitch_data = {0.5F, 0.5F, 2.0F, 0.5F, 1.0F, 1.0F};
+        Sound[] sound_data = {Sound.ITEM_BUCKET_FILL_FISH, Sound.BLOCK_NOTE_BLOCK_BASS, Sound.BLOCK_NOTE_BLOCK_BELL, Sound.ENTITY_PLAYER_LEVELUP, Sound.UI_TOAST_CHALLENGE_COMPLETE, Sound.UI_TOAST_CHALLENGE_COMPLETE};
         player.playSound(player.getLocation(), sound_data[item_class_code], 1.0F, pitch_data[item_class_code]);
 
         // 2. Broadcast Information
-        String[] item_class_styles = {"§3", "§7", "§b", "§d", "§e"};
+        String[] item_class_styles = {"§3", "§7", "§b", "§d", "§e", "§c§l"};
         String name_kor = pluginInstance.getConfig().getString("Caught." + item_class + "." + caught_material_name + ".name");
-        if (item_class_code < 3) player.sendMessage(item_class_styles[item_class_code] + item_class + " §f" + name_kor + " 을(를) 낚았다.");
-        else Bukkit.broadcastMessage(item_class_styles[item_class_code] + item_class + "! §f" + player.getName() + "님이 " + name_kor + " §f을(를) 낚았습니다!");
+        String batchim = "를";
+        if (pluginInstance.getConfig().getInt("Caught." + item_class + "." + caught_material_name + ".reward") % 2 == 1) {batchim = "을";}
+        if (item_class_code < 3) player.sendMessage(item_class_styles[item_class_code] + item_class + " §f" + name_kor + batchim +" 낚았다.");
+        else Bukkit.broadcastMessage(item_class_styles[item_class_code] + item_class + "! §f" + player.getName() + "님이 " + name_kor + batchim +" 낚았습니다!");
 
         // 3. Grant Experience Points
         manager.addExp(player, LevelManager.ExpCat.FISHING, pluginInstance.getConfig().getInt("Caught." + item_class + "." + caught_material_name + ".reward"));
@@ -126,7 +133,28 @@ public class PlayerFishEventListener implements Listener {
         else if (item_class.equals("RARE") && name_kor.equals("마법이 부여된 활")) stack.setItemStack(randomEnchantment(new ItemStack(Material.BOW)));
         else if (item_class.equals("LEGENDARY") && name_kor.equals("마법이 부여된 네더라이트 곡괭이")) stack.setItemStack(randomEnchantment(new ItemStack(Material.NETHERITE_PICKAXE)));
         else if (item_class.equals("LEGENDARY") && name_kor.equals("마법이 부여된 네더라이트 투구")) stack.setItemStack(randomEnchantment(new ItemStack(Material.NETHERITE_HELMET)));
-        else if (item_class.equals("LEGENDARY") && name_kor.equals("무한 수선 활")) stack.setItemStack(infMendEnchantment(new ItemStack(Material.BOW)));
+        else if (item_class.equals("SPECIAL") && name_kor.equals("무한 수선 활")) {
+            stack.setItemStack(infMendEnchantment(new ItemStack(Material.BOW)));
+        }
+        else if (item_class.equals("SPECIAL") && name_kor.equals("철 장검")) {
+            stack.setItemStack(attributeModifier(new ItemStack(Material.IRON_SWORD),2, EquipmentSlot.HAND,Attribute.PLAYER_ENTITY_INTERACTION_RANGE, AttributeModifier.Operation.ADD_NUMBER));
+        }
+        /*
+        else if (item_class.equals("SPECIAL") && name_kor.equals("저중력 부츠")) {
+            ItemStack boots = new ItemStack(Material.CHAINMAIL_BOOTS);
+            stack.setItemStack(attributeModifier(boots,-0.5, EquipmentSlot.FEET,Attribute.GENERIC_GRAVITY, AttributeModifier.Operation.MULTIPLY_SCALAR_1));
+            stack.setItemStack(attributeModifier(boots,-0.5, EquipmentSlot.FEET,Attribute.GENERIC_JUMP_STRENGTH, AttributeModifier.Operation.ADD_NUMBER));
+            stack.setItemStack(attributeModifier(boots,-0.5, EquipmentSlot.FEET,Attribute.GENERIC_GRAVITY, AttributeModifier.Operation.MULTIPLY_SCALAR_1));
+        }
+         */
+        else if (item_class.equals("SPECIAL") && name_kor.equals("신기전")) {
+            stack.setItemStack(addEnchantment(new ItemStack(Material.CROSSBOW),Enchantment.MULTISHOT,3));
+        }
+        else if (item_class.equals("SPECIAL") && name_kor.equals("자수정 검")) {
+            ItemStack item = new ItemStack(Material.AMETHYST_SHARD);
+            stack.setItemStack(attributeModifier(item,8, EquipmentSlot.HAND,Attribute.GENERIC_ATTACK_DAMAGE, AttributeModifier.Operation.ADD_NUMBER));
+            stack.setItemStack(addEnchantment(item,Enchantment.SHARPNESS,6));
+        }
         else stack.setItemStack(new ItemStack(Material.valueOf(caught_material_name)));
     }
 
@@ -154,11 +182,25 @@ public class PlayerFishEventListener implements Listener {
     }
 
     public ItemStack infMendEnchantment(ItemStack item) {
-        item.addUnsafeEnchantment(Enchantment.POWER, (int) Math.ceil(Math.random()*5));
-        item.addUnsafeEnchantment(Enchantment.FLAME, 1);
         item.addUnsafeEnchantment(Enchantment.INFINITY, 1);
         item.addUnsafeEnchantment(Enchantment.MENDING, 1);
-        item.addUnsafeEnchantment(Enchantment.KNOCKBACK, (int) Math.ceil(Math.random()*2));
+        return item;
+    }
+
+    public ItemStack addEnchantment(ItemStack item, Enchantment enchantment, int level) {
+        item.addUnsafeEnchantment(enchantment,level);
+        return item;
+    }
+
+    public ItemStack attributeModifier(ItemStack item, double amount, EquipmentSlot slot, Attribute attribute, AttributeModifier.Operation operation) {
+
+        ItemMeta meta = item.getItemMeta();
+
+        NamespacedKey key = attribute.getKey();
+        AttributeModifier modifier = new AttributeModifier(key, amount, operation, slot.getGroup());
+        meta.addAttributeModifier(attribute, modifier);
+
+        item.setItemMeta(meta);
         return item;
     }
 }
